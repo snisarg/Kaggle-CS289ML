3fd25b61a178c659adfc3845d08a5deae03f987e
Nisarg Shah <snisarg@gmail.com>
2016-03-06 14:33:57 -0800

    Bids for which we have user data

--- experiments/pandas_play.py
+++ experiments/pandas_play.py
@@ -4,13 +4,16 @@ import matplotlib
 bids = pandas.read_csv('../data/bids.csv', parse_dates=['time']).sort_values(by=['auction', 'time'])
 train = pandas.read_csv('../data/train.csv')
 
-#print bids
+print bids
 print bids['merchandise'].value_counts()
 bids['merchandise'].value_counts().plot(kind='bar')
 
 
 merged = pandas.merge(left=bids, right=train, how='left', left_on='bidder_id', right_on='bidder_id')
 print merged
+
+print merged['outcome'].value_counts()
+# We therefore have train data for 3071224 bids whether they're bots or humans.
 
 
 matplotlib.pyplot.show()
