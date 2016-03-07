cfd8d492bcd4cbd25a707bd1ecf129c84ae02ed1
Nisarg Shah <snisarg@gmail.com>
2016-03-07 10:16:17 -0800

    modifying values of columns

--- experiments/add_column.py
+++ experiments/add_column.py
@@ -1,7 +1,18 @@
 import pandas
 
 train = pandas.read_csv('../data/train.csv')
-print train
 
 train['new_col'] = 1
+print train
+
+newer_col = []
+flip = False
+for row in range(len(train)):
+    if flip:
+        newer_col.append(2)
+    else:
+        newer_col.append(1)
+    flip = not flip
+
+train.new_col = newer_col
 print train
