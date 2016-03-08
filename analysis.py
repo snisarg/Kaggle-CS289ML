c93c19d5f6e00104f6506aeb0c1e1643f6649ab1
Nisarg Shah <snisarg@gmail.com>
2016-03-07 17:43:12 -0800

    Seeing self outbid correlation. File generation commented

--- analysis.py
+++ analysis.py
@@ -22,7 +22,29 @@ get_inner_join = pool.apply_async(
 
 annot_bids = pandas.merge(left=bids, right=train, how='left', left_on='bidder_id', right_on='bidder_id')
 print annot_bids[['auction', 'bidder_id', 'outcome', 'time']]
+# annot_bids[['auction', 'bidder_id', 'outcome', 'time']].to_csv('data/temp.csv', sep='\t')
 # Looks like the bot bidder outbids his own highest bid
+# Self outbid column addition :
+# UNCOMMENT TO GENERATE FILE. IT TAKES A WHILE
+# last_auction_id = -1
+# self_outbid = []
+# for row_index in range(len(annot_bids)):
+#     print row_index
+#     if last_auction_id != annot_bids.loc[row_index]['auction']:
+#         last_auction_id = annot_bids.loc[row_index]['auction']
+#         self_outbid.append(0)
+#     else:
+#         if annot_bids.loc[row_index-1]['bidder_id'] == annot_bids.loc[row_index]['bidder_id']:
+#             self_outbid.append(1)
+#         else:
+#             self_outbid.append(0)
+#
+# annot_bids['self_outbid'] = self_outbid
+# annot_bids.to_csv('data/self_bid.csv', sep='\t')
+# print annot_bids['self_outbid'].value_counts()
+# 0    5582005
+# 1    2074329
+# Name: self_outbid, dtype: int64
 
 # Looking to see if country changes signify something
 country_user = get_inner_join.get()[['bidder_id', 'country', 'outcome']]
