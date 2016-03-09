084da4f244d1d0be4be9f18298bbd111fe1dac6a
Nisarg Shah <snisarg@gmail.com>
2016-03-08 18:44:38 -0800

    Generating bids and auctions count

--- analysis.py
+++ analysis.py
@@ -7,10 +7,10 @@ get_bids = pool.apply_async(
     lambda: pandas.read_csv('data/bids.csv', parse_dates=['time']).sort_values(by=['auction', 'time']))
 
 train = pandas.read_csv('data/train.csv')
-print train['outcome'].value_counts()
+# print train['outcome'].value_counts()
 
 # Trying to see if there are multiple users with the same address
-print train['address'].value_counts()
+# print train['address'].value_counts()
 # TO DO
 
 # Match bids and train set
@@ -20,8 +20,9 @@ get_inner_join = pool.apply_async(
     lambda: pandas.merge(left=bids, right=train, left_on='bidder_id', right_on='bidder_id')
                   .sort_values(by=['bidder_id']))
 
-annot_bids = pandas.merge(left=bids, right=train, how='left', left_on='bidder_id', right_on='bidder_id')
-print annot_bids[['auction', 'bidder_id', 'outcome', 'time']]
+# annot_bids = pandas.merge(left=bids, right=train, how='left', left_on='bidder_id', right_on='bidder_id')
+# print annot_bids[['auction', 'bidder_id', 'outcome', 'time']]
+
 # annot_bids[['auction', 'bidder_id', 'outcome', 'time']].to_csv('data/temp.csv', sep='\t')
 # Looks like the bot bidder outbids his own highest bid
 # Self outbid column addition :
@@ -47,7 +48,20 @@ print annot_bids[['auction', 'bidder_id', 'outcome', 'time']]
 # Name: self_outbid, dtype: int64
 
 # Looking to see if country changes signify something
-country_user = get_inner_join.get()[['bidder_id', 'country', 'outcome']]
-print country_user[country_user['outcome']==0].drop_duplicates()
+# country_user = get_inner_join.get()[['bidder_id', 'country', 'outcome']]
+# print country_user[country_user['outcome']==0].drop_duplicates()
 
 # print train[train['outcome'] == 1]
+
+# bid_per_user = annot_bids['bidder_id'].value_counts()
+# print pandas.merge(left=bid_per_user, right=train, left_on='bidder_id', right_on='bidder_id')
+inner_join = get_inner_join.get()
+# inner_join['bid_count'] = inner_join.groupby(by='bidder_id', as_index=False)['bidder_id'].transform(
+#     lambda s: (float(s.count()-1)/515032.0))
+# print inner_join[['bidder_id', 'bid_count', 'outcome']].sort_values(by=['bid_count']).drop_duplicates()
+
+print inner_join[['bidder_id', 'auction']].drop_duplicates()['bidder_id'].value_counts()
+
+inner_join['auction_count'] = inner_join[['bidder_id', 'auction']].drop_duplicates().groupby(by=['bidder_id'], as_index=False)['auction'].transform('count')
+res = inner_join[['bidder_id', 'auction_count', 'outcome']].sort_values(by=['auction_count'], ascending=False).drop_duplicates()
+print res[res['auction_count'] > -1]
