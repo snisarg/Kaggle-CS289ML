abb623506566b5db637c51134b3a1c676d9ca9ed
Nisarg Shah <snisarg@gmail.com>
2016-03-09 11:07:19 -0800

    Filtering to variables to ease imports

--- analysis.py
+++ analysis.py
@@ -1,10 +1,10 @@
 import pandas
 from multiprocessing.pool import ThreadPool
 
-pool = ThreadPool(processes=4)
+#pool = ThreadPool(processes=4)
 
-get_bids = pool.apply_async(
-    lambda: pandas.read_csv('data/bids.csv', parse_dates=['time']).sort_values(by=['auction', 'time']))
+#get_bids = pool.apply_async(
+#    lambda: pandas.read_csv('data/bids.csv', parse_dates=['time']).sort_values(by=['auction', 'time']))
 
 train = pandas.read_csv('data/train.csv')
 # print train['outcome'].value_counts()
@@ -14,11 +14,11 @@ train = pandas.read_csv('data/train.csv')
 # TO DO
 
 # Match bids and train set
-bids = get_bids.get()
+bids = pandas.read_csv('data/bids.csv', parse_dates=['time']).sort_values(by=['auction', 'time'])
 
-get_inner_join = pool.apply_async(
-    lambda: pandas.merge(left=bids, right=train, left_on='bidder_id', right_on='bidder_id')
-                  .sort_values(by=['bidder_id']))
+# get_inner_join = pool.apply_async(
+#     lambda: pandas.merge(left=bids, right=train, left_on='bidder_id', right_on='bidder_id')
+#                   .sort_values(by=['bidder_id']))
 
 # annot_bids = pandas.merge(left=bids, right=train, how='left', left_on='bidder_id', right_on='bidder_id')
 # print annot_bids[['auction', 'bidder_id', 'outcome', 'time']]
@@ -55,21 +55,23 @@ get_inner_join = pool.apply_async(
 
 # bid_per_user = annot_bids['bidder_id'].value_counts()
 # print pandas.merge(left=bid_per_user, right=train, left_on='bidder_id', right_on='bidder_id')
-inner_join = get_inner_join.get()
+inner_join = pandas.merge(left=bids, right=train, left_on='bidder_id', right_on='bidder_id') \
+                   .sort_values(by=['bidder_id'])
 
 # Number of bids per bidder
-# inner_join['bid_count'] = inner_join.groupby(by='bidder_id', as_index=False)['bidder_id'].transform(
-#     lambda s: (float(s.count()-1)/515032.0))
-# print inner_join[['bidder_id', 'bid_count', 'outcome']].sort_values(by=['bid_count']).drop_duplicates()
+inner_join['bid_count'] = inner_join.groupby(by='bidder_id', as_index=False)['bidder_id'].transform(
+    lambda s: (float(s.count()-1)/515032.0))
+bid_count = inner_join[['bidder_id', 'bid_count', 'outcome']].sort_values(by=['bid_count']).drop_duplicates()
 
-# print inner_join[['bidder_id', 'auction']].drop_duplicates()['bidder_id'].value_counts()
+#print inner_join[['bidder_id', 'auction']].drop_duplicates()['bidder_id'].value_counts()
 
 # Number of auctions per bidder participated
-# inner_join['auction_count'] = inner_join[['bidder_id', 'auction']].drop_duplicates().groupby(by=['bidder_id'], as_index=False)['auction'].transform(
-#     lambda s: float(s.count()-1)/1622.0
-# )
-# res = inner_join[['bidder_id', 'auction_count', 'outcome']].sort_values(by=['auction_count'], ascending=False).drop_duplicates()
-# print res[res['auction_count'] > -1]
+inner_join['auction_count'] = inner_join[['bidder_id', 'auction']].drop_duplicates().groupby(by=['bidder_id'], as_index=False)['auction'].transform(
+    lambda s: float(s.count()-1)/1622.0
+)
+auction_counts = inner_join[['bidder_id', 'auction_count', 'outcome']].sort_values(by=['auction_count'], ascending=False).drop_duplicates()
+auction_counts = auction_counts[auction_counts['auction_count'] > -1]
+# print auction_counts
 
 # print inner_join[['bidder_id', 'auction_count']]
 
@@ -77,24 +79,24 @@ inner_join = inner_join.sort_values(by=['bidder_id', 'time'])
 #inner_join.to_csv('data/sorted_inner_join.csv')
 # print inner_join[['bidder_id', 'time']]
 
-last_bidder_id = 'abc'
-prev_row = 0
-time_diff = []
-for index, row in inner_join.iterrows():
-    # print row_index
-    if last_bidder_id != row['bidder_id']:
-        last_bidder_id = row['bidder_id']
-        time_diff.append(0)
-    else:
-        # if inner_join.loc[row_index-1]['bidder_id'] == inner_join.loc[row_index]['bidder_id']:
-        difference = float(row['time']) - float(prev_row['time'])
-        if difference < 0:
-            print prev_row
-            print row
-        time_diff.append(difference)
-        #else:
-        #    self_outbid.append(0)
-    prev_row = row
-
-inner_join['time_diff'] = time_diff
-inner_join.to_csv('data/time_diff.csv', sep='\t')
+# last_bidder_id = 'abc'
+# prev_row = 0
+# time_diff = []
+# for index, row in inner_join.iterrows():
+#     # print row_index
+#     if last_bidder_id != row['bidder_id']:
+#         last_bidder_id = row['bidder_id']
+#         time_diff.append(0)
+#     else:
+#         # if inner_join.loc[row_index-1]['bidder_id'] == inner_join.loc[row_index]['bidder_id']:
+#         difference = float(row['time']) - float(prev_row['time'])
+#         if difference < 0:
+#             print prev_row
+#             print row
+#         time_diff.append(difference)
+#         #else:
+#         #    self_outbid.append(0)
+#     prev_row = row
+#
+# inner_join['time_diff'] = time_diff
+# inner_join.to_csv('data/time_diff.csv', sep='\t')
