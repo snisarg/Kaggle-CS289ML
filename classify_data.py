86b88c669bd008caf1448f15e37ce601a18ab05d
Nisarg Shah <snisarg@gmail.com>
2016-03-10 10:12:47 -0800

    Neural networks for classification

--- classify_data.py
+++ classify_data.py
@@ -1,5 +1,5 @@
 import pandas
-from sklearn import linear_model, cross_validation
+from sklearn import linear_model, cross_validation, neural_network
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestRegressor
 import numpy
@@ -47,3 +47,15 @@ def svm(x, y):
     print 'Mean RMSE',  numpy.mean(numpy.sqrt(-scores))
     print 'Best RMSE',  numpy.min(numpy.sqrt(-scores))
     print 'Coefficients', model.feature_importances_
+
+
+def neural_networks(x, y):
+    model = neural_network.MLPRegressor([1,1,1,1,1], 'relu', 'adam', 0.0001, 200, 'constant', 0.001, 0.5, 200,
+                                        True, None, 0.0001, False, False, 0.9, True, False, 0.1, 0.9, 0.999, 1e-08)
+    predicted = cross_validation.cross_val_predict(model, x, y, 10, 1, 0, None, 0)
+    scores = cross_validation.cross_val_score(model, x, y,  cv=10, scoring='mean_squared_error')
+
+    print 'All RMSEs',  numpy.sqrt(-scores)
+    print 'Mean RMSE',  numpy.mean(numpy.sqrt(-scores))
+    print 'Best RMSE',  numpy.min(numpy.sqrt(-scores))
+    print 'Coefficients', model.get_params(True)
