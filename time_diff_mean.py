808498910b68d102432f441e3e4194b5de0b8eb7
mansi512 <mansi1.618@gmail.com>
2016-03-11 23:02:18 -0800

    Added for test features also, final commit

--- time_diff_mean.py
+++ time_diff_mean.py
@@ -2,9 +2,16 @@ import pandas
 from sklearn import linear_model, cross_validation
 import numpy
 
+#For Train Data
 self_bid = pandas.read_csv('data/time_diff.csv', sep='\t')
 self_bid['time_diff_mean'] = self_bid.groupby(by='bidder_id', as_index=False)['time_diff'].transform('mean')
 
 sorted_time_diff = self_bid[['bidder_id', 'time_diff_mean', 'outcome']].drop_duplicates().sort_values('time_diff_mean')
 
-print sorted_time_diff[sorted_time_diff['outcome']==1]
+#For Test Data
+self_bid = pandas.read_csv('data/test_time_diff.csv', sep='\t')
+self_bid['time_diff_mean'] = self_bid.groupby(by='bidder_id', as_index=False)['time_diff'].transform('mean')
+
+test_sorted_time_diff = self_bid[['bidder_id', 'time_diff_mean']].drop_duplicates().sort_values('time_diff_mean')
+
+# print sorted_time_diff[sorted_time_diff['outcome']==1]
