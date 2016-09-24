92cff8c6f62e83e71979422aa89d3d5037af4872
Nisarg Shah <snisarg@gmail.com>
2016-09-24 16:04:04 -0700

    SMOTE implemented

---
+++ smote_resampling.py
@@ -0,0 +1,20 @@
+import pandas
+from imblearn.combine import smote_enn
+
+sme = smote_enn.SMOTEENN(random_state=42)
+
+final_col = pandas.read_csv('data/final_col.csv')
+
+X, Y = sme.fit_sample(
+    final_col[['outbid_count','time_diff_mean', 'bid_count', 'auction_count','device_count','ip_count',
+               'country_count','dev_ent']],
+    final_col['outcome'])
+
+print Y
+print X
+
+smote_col = pandas.DataFrame(X, columns=['outbid_count', 'time_diff_mean', 'bid_count', 'auction_count',
+                                       'device_count', 'ip_count', 'country_count', 'dev_ent'])
+smote_col['outsome'] = Y
+
+smote_col.to_csv('data/smote_col.csv')
