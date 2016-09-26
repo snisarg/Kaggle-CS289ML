3e9cbaa307f558118ec1e1e74809c3087e93aa29
Nisarg Shah <snisarg@gmail.com>
2016-09-25 20:19:04 -0700

    Graph generation for newer models

--- operations.py
+++ operations.py
@@ -14,7 +14,7 @@ final_col = pandas.read_csv('data/smote_col.csv')
 #     final_col['outcome'])
 
 
-classify_data.grid_svm(
+classify_data.grid_random_forest(
     final_col[['outbid_count','time_diff_mean', 'bid_count', 'auction_count','device_count','ip_count',
                'country_count','dev_ent']],
     final_col['outcome'])
